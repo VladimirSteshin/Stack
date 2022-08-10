ebf1e0a73f3def286e7b1f3d09616b72444467e3
Steshin <@gs.pmok>
2022-08-10 12:55:17 +0300

    checked

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from task import rows
 if __name__ == '__main__':
     brackets = Stack()
     for row in rows:
-        brackets.stack = []
+        brackets.stack = []  # cleaning stack
         if row:
-            good = True
+            good = True  # creating condition for the final check below
         else:
             good = False
         for item in row:
-            if item in '([{':
+            if item in '([{':  # finding left bracket
                 brackets.push(item)
-            elif item in ')]}':
+            elif item in ')]}':  # finding right bracket
                 if brackets.is_empty():
                     good = False
                     break
                 else:
-                    left_bracket = brackets.pop()
+                    left_bracket = brackets.pop()  # take the upper bracket from stack and compare
                     if left_bracket == '(' and item == ')':
                         continue
                     elif left_bracket == '[' and item == ']':
@@ -30,7 +30,7 @@ if __name__ == '__main__':
             else:
                 good = False
                 break
-        if good and brackets.size() == 0:
+        if good and brackets.size() == 0:  # if our condition status is 'good' and the stack is clean
             print('Balanced')
         else:
             print('Unbalanced')
